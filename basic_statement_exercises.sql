6b4ba12f48d2fb650b1c08cc300981ebf29f83b9
mannievilla <mannievilla@gmail.com>
2023-05-02 11:54:29 -0500

    add the answers not just the query to questions

--- basic_statement_exercises.sql
+++ basic_statement_exercises.sql
@@ -20,30 +20,64 @@ SELECT name
 FROM albums
 WHERE artist = 'Pink Floyd'
 ;
+-- Dark Side of the Moon
+-- The Wall
 
 -- 6
 SELECT release_date
 FROM albums
 WHERE name LIKE 'Sgt.%'
 ;
+-- 1967
 
 -- 7
 SELECT genre
 FROM albums
 WHERE name = 'Nevermind'
 ;
+-- Grunge, Alternative rock
 
 -- 8
 SELECT name
 FROM albums
 WHERE release_date BETWEEN 1989 AND 1999
 ;
+-- The Bodyguard
+-- Jagged Little Pill
+-- Come On Over
+-- Falling into You
+-- Let's Talk About Love
+-- Dangerous
+-- The Immaculate Collection
+-- Titanic: Music from the Motion Picture
+-- Metallica
+-- Nevermind
+-- Supernatural
 
 -- 9 
 SELECT name AS low_selling_albums
 FROM albums
 WHERE sales < 20.0
 ;
+-- Grease: The Original Soundtrack from the Motion Picture
+-- Bad
+-- Sgt. Pepper's Lonely Hearts Club Band
+-- Dirty Dancing
+-- Let's Talk About Love
+-- Dangerous
+-- The Immaculate Collection
+-- Abbey Road
+-- Born in the U.S.A.
+-- Brothers in Arms
+-- Titanic: Music from the Motion Picture
+-- Nevermind
+-- The Wall
+
 -- SELECT *
 -- FROM albums
 -- ; 
+
+
+
+
+
